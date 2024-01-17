e9ce35bb250346362d21369d81b7b7f443cf50b7
divyanshu3725 <divyanshu3725@gmail.com>
2024-01-17 12:25:23 +0530

    day1

---
+++ script.js
@@ -0,0 +1,17 @@
+// DATA TYPES
+// let a = 1;
+// let b = true;
+// let c = 'string';
+// let d = function(){
+//     console.log("Hi");
+// };
+// let e = {};
+// let f = [];
+// let g = new Date();
+// console.log(a, typeof(a));
+// console.log(b, typeof(b));
+// console.log(c, typeof(c));
+// console.log(d, typeof(d));
+// console.log(e, typeof(e));
+// console.log(f, typeof(f));
+// console.log(g, typeof(g));
